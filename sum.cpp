cc7e494499482f959c505b6203e9a4706f97f93b
shubhamdavare-hub <shubhamdavare2513@gmail.com>
2026-07-17 07:29:58 +0000

    sum

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"enter 2 numbers=";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"addition="<<a<<"+"<<b<<"="<<sum;
+    return 0;
+}
