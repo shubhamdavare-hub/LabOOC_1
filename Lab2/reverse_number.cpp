511c993c217a9d1a3d89d0967fe80bba963e0570
shubhamdavare-hub <shubhamdavare2513@gmail.com>
2026-07-30 15:04:10 +0000

    reverse a number

---
+++ Lab2/reverse_number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int num, reversedNum = 0, remainder;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+
+    int original = num; // Keep original copy if needed
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+
+    std::cout << "Reversed Number = " << reversedNum << std::endl;
+
+    return 0;
+}
