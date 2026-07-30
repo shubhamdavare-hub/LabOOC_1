8927b307c75134314efea47be34be4fa6f389a16
shubhamdavare-hub <shubhamdavare2513@gmail.com>
2026-07-30 15:19:19 +0000

    name,roll no,Div

---
+++ C_code/name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Shubham Davare\n");
+    printf("Roll No: 1\n");
+    printf("Class Division: c\n");
+    return 0;
+}scm-history-item:%5Cworkspaces%5CLabOOC_1?%7B%22repositoryId%22%3A%22scm0%22%2C%22historyItemId%22%3A%22511c993c217a9d1a3d89d0967fe80bba963e0570%22%2C%22historyItemParentId%22%3A%228301e786f686785f70b48a129de6c4e8efa338c9%22%2C%22historyItemDisplayId%22%3A%22511c993%22%7D
