798e030e7cf68733c47cc4530a607d9e8761d497
shubhamdavare-hub <shubhamdavare2513@gmail.com>
2026-07-31 07:35:31 +0000

    manipulators

---
+++ Lab3_30/7/manipulators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main()
+{
+    int basic=950,Allowance=95,Total=545;
+    cout<<setw(5)<<"basic"<<setw(5)<<basic<<endl<<setw(5)<<Allowance<<endl<<setw(5)<<"Total"<<setw(5)<<Total<<endl;
+    return 0;
+}
