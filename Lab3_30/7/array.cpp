4cd57a0e055f4e6b5b25f01a62ba2311de400b19
shubhamdavare-hub <shubhamdavare2513@gmail.com>
2026-07-31 06:55:58 +0000

    sum of element present in array

---
+++ Lab3_30/7/array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int *arr;
+    int size,sum=0;
+
+    cout<<"Enter the size of the integer array:";
+    cin>>size;
+    
+    arr= new int[size];
+    cout<<"Creating an array of size"<<size<<"..";
+   
+    
+    for(int i=0;i<size;i++){
+        cout<<"Enter element : ";
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    cout<<"nsum of all elements = "<<sum<<endl;
+    delete arr;
+    return 0;
+    
+}
